bcfbc648be26068161d3ac7e833d0109a353ee88
Nikhil VK <nikhil.vakkiprathkodakkadan@medtronic.com>
2021-07-19 19:07:41 +0530

    Modified forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const getForcaste = (latitude, longitude, callback) => {
     const url =
         'http://api.weatherstack.com/current?access_key=15fb2cb162e2fe4ed6ea1df64eb05551&query=' + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             console.log('Error : ' + error)
             callback('Unable to connect to weather services!', undefined)
@@ -14,7 +14,8 @@ const getForcaste = (latitude, longitude, callback) => {
                 undefined)
         } else {
             console.log('Success')
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like '
+                + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
